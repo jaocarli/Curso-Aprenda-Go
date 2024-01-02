85f720a2733d6565e9418c3d97a08310503cc4ad
João Carli <jthorlief@gmail.com>
2024-01-02 16:25:09 -0300

    first commit

---
+++ variáveis-valores-tipos/01 - helloworld/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	numerodebytes, erro := fmt.Println("Hello World!")
+	fmt.Println(numerodebytes, erro)
+
+	x := 16
+	y := "strings"
+	z := true
+
+	fmt.Println(x, y, z)
+}
